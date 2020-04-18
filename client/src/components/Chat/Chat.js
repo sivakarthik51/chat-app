6a89c34a0f06f036652750ade2359752dd73cead
Siva Karthik <siramesh@akamai.com>
2020-04-18 23:06:24 +0530

    Added Basic Chat Component

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,12 +1,18 @@
 import React,{useState,useEffect} from 'react';
 import queryString from 'query-string';
 import io from 'socket.io-client';
+import {Input} from 'antd';
+import InfoBar from '../InfoBar/InfoBar';
+
+import './Chat.css';
 
 let socket;
 
 const Chat = ({ location }) => {
     const [name,setName] = useState('');
     const [room,setRoom] = useState('');
+    const [message,setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
 
     const ENDPOINT =  'localhost:5000';
     useEffect(() => {
@@ -25,8 +31,28 @@ const Chat = ({ location }) => {
         }
         
     },[ENDPOINT,location.search])
+
+    useEffect(() => {
+        socket.on('message', message =>{
+            setMessages(messages => [...messages,message]);
+        })
+    },[])
+
+    const sendMessage = (event) => {
+        event.preventDefault();
+        if(message) {
+            socket.emit('sendMessage',message,() => setMessage(''));
+        }
+    }
+    console.log(message,messages);
     return (
-        <h1>Chat</h1>
+        <div className="outerContainer">
+            <div className="container">
+                {/* <Input value={message} placeholder="Enter Message" onChange={(event) => setMessage(event.target.value)} onPressEnter={(event) => sendMessage(event)}/> */}
+                <InfoBar room = {room}/>
+            </div>
+
+        </div>
     );
 }
 
